5f10413d56360687376562a236862d64a8dbae79
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-07-21 14:48:05 +0700

    feat: router to logout

--- routes/auth.js
+++ routes/auth.js
@@ -45,4 +45,11 @@ router.post('/login', async(req, res) => {
     }
 })
 
+router.post('/logout', async(req, res) => {
+    req.session.destroy(err => {
+        if (err) return res.redirect('/')
+        res.redirect('/')
+    }) 
+})
+
 module.exports = router
