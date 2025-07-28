b617e05a04f39c45f33c8f760e1da853100e9df6
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-07-28 14:59:22 +0700

    fix: CRUD in rak

--- routes/pengurus/lokasi/rak.js
+++ routes/pengurus/lokasi/rak.js
@@ -2,50 +2,89 @@ const express = require('express')
 const router = express.Router()
 //import model rak 
 const modelRak = require('../../../model/modelRak')
-
+//import model ruangan untuk menampilkan data ruanga
+const modelRuangan = require('../../../model/modelRuangan')
 //menampilakn semua data rak
 router.get('/', async(req, res) => {
     try {
-        let data = modelRak.getAll()
-        res.render('pengurus/user/lokasi/rak/index')
+        let data = await modelRak.getAll()
+        res.render('pengurus/user/lokasi/rak/index', {data})
     } catch(err) {
-        req.flash(err)
+        req.flash('error', err.message)
+    }
+})
+
+router.get('/buat', async (req, res) => {
+    try {
+        const ruangan = await modelRuangan.getAll()
+        res.render('pengurus/user/lokasi/rak/buat', {ruangan})
+    } catch (err) {
+        req.flash('error', err.message)
+        res.redirect('/pengurus/rak')
     }
 })
 
 //menabahkan data rak baru
-router.post('/buat', async(req, res) => {
+router.post('/create', async(req, res) => {
     try {
-        let {kodeRak, idRuangan} = req.body
-        let data = {kodeRak, idRuangan}
+        let {id_ruangan, kode_rak} = req.body
+        let data = {id_ruangan, kode_rak}
+        const checkRak = await modelRak.checkRak(data)
+        if (checkRak) {
+            req.flash('error', 'Kode Rak sudah ada')
+            return res.redirect('/pengurus/rak/buat')
+        }
         await modelRak.store(data)
-        res.render('pengurus/user/lokasi/rak/index')
+        res.redirect('/pengurus/rak')
     } catch(err) {
-        req.flash(err)
+        req.flash('error', err.message)
+        res.redirect('/pengurus/rak')
+    }
+})
+
+router.get('/edit/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+        const rak = await modelRak.getById(id)
+        const ruangan = await modelRuangan.getAll()
+        res.render('pengurus/user/lokasi/rak/edit', {rak, ruangan})
+    } catch(err) {
+        req.flash('error', err.message)
+        res.redirect('/pengurus/rak')
     }
 })
 
 //memgupdate data rak berdasarkan id
-router.post('/edit/:id', async(req, res) => {
+router.post('/update/:id', async(req, res) => {
     try {
-        let id = req.params
-        let {kodeRak, idRuangan} = req.body
-        let data = {kodeRak, idRuangan}
+        const {id} = req.params
+        const {kode_rak, id_ruangan} = req.body
+        const data = {kode_rak, id_ruangan}
+        const rak = await modelRak.getById(id)
+        const checkRak = await modelRak.checkRak(data)
+        if (checkRak) {
+            req.flash('error', 'Kode Rak sudah ada')
+            return res.redirect(`/pengurus/rak/edit/${rak[0].id}`)
+        }
         await modelRak.update(data, id)
-        res.render('pengurus/user/lokasi/rak/index')
+        req.flash('success', 'Data berhasil Diupdate')
+        res.redirect('/pengurus/rak')
     } catch(err) {
-        req.flash(err)
+        req.flash('error', err.message)
+        res.redirect('/pengurus/rak')
     }
 })
 
 //mengapus data rak berdasarakn id
 router.post('/delete/:id', async (req, res) => {
     try {
-        let id = req.params
+        const {id} = req.params
         await modelRak.delete(id)
-        res.render('pengurus/user/lokasi/rak/index')
+        req.flash('success', 'Data Berhasil Dihapus')
+        res.redirect('/pengurus/rak')
     } catch(err) {
-        req.flash(err)
+        req.flash('error', err.message)
+        res.redirect('/pengurus/rak')
     }
 })
 
