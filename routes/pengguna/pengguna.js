b2165f865a97ec93c7cc57d6652185adb1d7d6df
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-09-18 12:07:04 +0700

    feat: get detail buku or majalah

--- routes/pengguna/pengguna.js
+++ routes/pengguna/pengguna.js
@@ -1,15 +1,41 @@
 const express = require('express')
 const router = express.Router()
 const modelBuku = require('../../model/modelBuku')
+const modelMajalah = require('../../model/modelMajalah')
 
 router.get('/', async (req, res) => {
+    res.render('pengguna/index')
+})
+
+router.post('/', async (req, res) => {
     try {
-        // const {keyboard} = req.body
-        // await modelBuku.getBukuDanMajalh(keyboard)
-        res.render('pengguna/index')
+        const {keyword} = req.body
+        const result = await modelBuku.getBukuAndMajalah(keyword)
+
+        res.render('pengguna/index', { result, keyword })
     } catch(err) {
         req.flash('error', err.msg)
     }
 })
+
+router.get('/detail/:tipe/:id', async (req, res) => {
+    try {
+        const { id, tipe } = req.params
+
+        if (tipe == 'Majalah') {
+            const rows = await modelMajalah.getDetailMajalah(id)
+            const majalah = rows[0]
+            res.render('pengguna/detail_majalah', { majalah })
+        } else if (tipe == 'Buku') {
+            const rows = await modelBuku.getDetailBuku(id)
+            const buku = rows[0]
+            res.render('pengguna/detail_buku', { buku })
+        }
+    } catch (err) {
+        req.flash('error', err.message)
+        res.redirect('/')
+    }
+})
+
 
 module.exports = router
