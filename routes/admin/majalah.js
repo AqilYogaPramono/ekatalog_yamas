a8352f2825e141444de98b0f6346de97bfc5e7d1
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-09-18 11:48:05 +0700

    feat: routes to get all majalah when status hapus and admin can update majalah to permanen delete or change status to show

---
+++ routes/admin/majalah.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const router = express.Router()
+const modelMajalah = require('../../model/modelMajalah')
+const path = require('path')
+const fs = require('fs')
+const {authAdmin} = require('../../middleware/auth')
+const modelPengurus = require('../../model/modelPengurus')
+
+const deleteOldPhoto = (oldPhoto) => {
+    if (oldPhoto) {
+        const filePath = path.join(__dirname, '../../public/images/majalah', oldPhoto)
+        if (fs.existsSync(filePath)) fs.unlinkSync(filePath)
+    }
+}
+
+router.get('/', authAdmin, async (req, res) => {
+    try {
+        const userId = req.session.pengurusId
+
+        const  user = await modelPengurus.getPengurusById(userId)
+
+        const majalah = await modelMajalah.getAllBukuHapus()
+
+        res.render('pengurus/admin/majalah/index', { majalah, user })
+    } catch (err) {
+        console.log(err)
+        req.flash('error', err.message)
+        res.redirect('/admin/dashboard')
+    }
+})
+
+router.post('/edit/:id', authAdmin, async (req, res) => {
+    try {
+        const {id} = req.params
+        
+        const {status_data} = req.body
+        const data = {status_data}
+
+        await modelMajalah.updateStatusData(data, id)
+
+        req.flash('success', 'Majalah berhasil ditampilkan')
+        res.redirect('/admin/majalah')
+    } catch(err) {
+        console.log(err)
+        req.flash('error', err.message)
+        res.redirect('/admin/majalah')
+    }
+})
+
+router.post('/delete/:id', authAdmin, async (req, res) => {
+    try {
+        const { id } = req.params
+        
+        const majalah = await modelMajalah.getById(id)
+        const oldPhoto = majalah.foto_cover
+
+        deleteOldPhoto(oldPhoto)
+        await modelMajalah.hardDelete(id)
+
+        req.flash('success', 'Majalah berhasil dihapus')
+        res.redirect('/admin/majalah')
+    } catch (err) {
+        req.flash('error', err.message)
+        res.redirect('/admin/majalah')
+    }
+})
+
+module.exports = router
