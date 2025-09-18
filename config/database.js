7b1558a773e3bdb8ff275c5640cd5f2888d6eab3
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-09-18 11:05:42 +0700

    chore: change libary msyql to mysql2

--- config/database.js
+++ config/database.js
@@ -1,15 +1,23 @@
-const mysql = require('mysql')
+const mysql = require('mysql2')
 
-database = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-})
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+}).promise();
 
-database.connect((err) => {
-    if(err) return console.log(err)
-    return console.log("Berhasil Terhubung")
-})
+(async () => {
+    try {
+        const conn = await pool.getConnection()
+        console.log('MySQL connected successfully!')
+        conn.release()
+    } catch (err) {
+        console.error('MySQL connection failed:', err)
+    }
+})()
 
-module.exports = database
+module.exports = pool
